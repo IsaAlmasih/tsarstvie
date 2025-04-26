ba9f067fdfbb927766c32a4d2643280121246b2e
Savva <imperiya66.ru@gmail.com>
2025-04-27 00:32:01 +0500

    DimaSavva

---
+++ src/app/Clouds/Clouds.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+function page() {
+  return (
+    <div className={styles.clouds}>
+      <div className={styles.clouds1}></div>
+      <div className={styles.clouds2}></div>
+      <div className={styles.clouds3}></div>
+    </div>
+  );
+}
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,44 @@
+"use client";
 import React from "react";
-import stules from "./page.module.css";
+import styles from "./page.module.css";
+
 import Link from "next/link";
 
-// import { MatrixRain } from "./../components/MatrixRain/MatrixRain";
+import Clouds from "./Clouds/Clouds"
+import logo from "./assets/kingdom.png";
 
 const page = () => {
   return (
-    <div>
-      <div className={stules.wrapper}>
-        <Link href="/" className={stules.text}>
-          Царствие.
-        </Link>
-      </div>
-      {/* <MatrixRain></MatrixRain> */}
+    <div className={styles.app}>
+      <Clouds></Clouds>
+      {/* Clouds from "./Clouds/Clouds"; */}
+      {/* Название класса.className */}
+      {/* <img src={logo} className={styles.Applogo} alt="logo" /> */}
+      {/* <div className={styles.title}>כס דוד</div> */}
+      <div className={styles.subtitle}>Царствие.</div>
+      {/* Кнопка и её стили.  */}
+      <Link href="/" className={styles.link}>
+        {/* <button className={styles.button}>كتب الأقدار</button> */}
+      </Link>
     </div>
   );
 };
 
 export default page;
+
+// import { MatrixRain } from "./../components/MatrixRain/MatrixRain";
+
+// const page = () => {
+//   return (
+//     <div>
+//       <div className={stules.wrapper}>
+//         <Link href="/" className={stules.text}>
+//           Царствие.
+//         </Link>
+//       </div>
+//       {/* <MatrixRain></MatrixRain> */}
+//     </div>
+//   );
+// };
+
+// export default page;
