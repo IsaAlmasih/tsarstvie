2fd1881706134f0a274675f9108950c2ce1dd62d
Savva <imperiya66.ru@gmail.com>
2025-05-03 21:59:46 +0500

    savva

--- src/app/page.js
+++ src/app/page.js
@@ -11,14 +11,7 @@ const page = () => {
   return (
     <div className={styles.app}>
       <Clouds></Clouds>
-      {/* Clouds from "./Clouds/Clouds"; */}
-      {/* Название класса.className */}
       <img src={logo.src} className={styles.Applogo} alt="logo" />
-      {/* <div className={styles.title}>כס דוד</div> */}
-      {/* Кнопка и её стили.  */}
-      <Link href="/page" className={styles.link}>
-        {/* <button className={styles.button}>كتب الأقدار</button> */}
-      </Link>
       <Link href="/tsarstvie" className={styles.subtitle}>Царствие.</Link>
     </div>
   );
@@ -26,19 +19,3 @@ const page = () => {
 
 export default page;
 
-// .app .Applogo .link .subtitle        import { MatrixRain } from "./../components/MatrixRain/MatrixRain";
-
-// const page = () => {
-//   return (
-//     <div>
-//       <div className={stules.wrapper}>
-//         <Link href="/" className={stules.text}>
-//           Царствие.
-//         </Link>
-//       </div>
-//       {/* <MatrixRain></MatrixRain> */}
-//     </div>
-//   );
-// };
-
-// export default page;
