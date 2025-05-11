e56bb2b024d4e5a8bef5589f41563ce3157c9eb0
Savva <imperiya66.ru@gmail.com>
2025-05-11 11:08:53 +0500

    savva

--- src/app/tsarstvie/page.jsx
+++ src/app/tsarstvie/page.jsx
@@ -8,13 +8,11 @@ const page = () => {
     <div className={styles.wrapper}>
       <div className={styles.blok}>
         <div className={styles.text}>
+          <Link href="/">Закон Всевышнего Иисуса Христа.</Link>
           <Link href="/tsarstvie/churches">Церкви.</Link>
-          <Link href="/">
-            Светильники.
-          </Link>
-          <Link href="/">
-            Закон Всевышнего Иисуса Христа.
-          </Link>
+          <Link href="/">Светильники.</Link>
+          <Link href="/">12 колен Израиля</Link>
+          <Link href="/">Вердикты.</Link>
           <Link href="/" className={styles.textTsar}>
             Вернуться на главную.
           </Link>
