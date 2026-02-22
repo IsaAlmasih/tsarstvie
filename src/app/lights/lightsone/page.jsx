6f3ab5f905d3509d3e0ebf835eec1a81247b10f3
Savva <imperiya66.ru@gmail.com>
2026-02-22 12:48:50 +0500

    Savva/SDS

--- src/app/lights/lightsone/page.jsx
+++ src/app/lights/lightsone/page.jsx
@@ -7,19 +7,18 @@ const page = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.blok}>
-        <div className={styles.text}>
-          <div className={styles.textTsar}>
-            <h1>Первая печать.</h1>
-            <h1 className={styles.suwyz}>
-              17) И я видел, что Агнец Снял первую из семи печатей, и я услышал
-              одно из четырёх животных, говорящее как бы громовым голосом: иди и
-              смотри. 18) Я взглянул, и вот, конь белый, на нём всадник, имеющий
-              лук, и дан был ему венец; и вышел он как победоносный, и чтобы
-              победить.
-            </h1>
-          </div>
-          <Link href="/blagosds">Благотворительный Фонд СДС и ОУ.</Link>
-          <Link href="/lights" className={styles.textTsar}>
+        <div className={styles.textTsar}>
+          <h1>
+            17) И я видел, что Агнец Снял первую из семи печатей, и я услышал
+            одно из четырёх животных, говорящее как бы громовым голосом: иди и
+            смотри. 18) Я взглянул, и вот, конь белый, на нём всадник, имеющий
+            лук, и дан был ему венец; и вышел он как победоносный, и чтобы
+            победить.
+          </h1>
+          <Link href="/blagosds" className={styles.ruxyz}>
+            Благотворительный Фонд СДС и ОУ.
+          </Link>
+          <Link href="/lights" className={styles.ruxyz}>
             Вернуться на главную.
           </Link>
         </div>
