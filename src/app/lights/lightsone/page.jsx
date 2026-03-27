54da111d7e5af199845a2aa2170febf02ea46357
Savva <imperiya66.ru@gmail.com>
2026-03-28 04:09:24 +0500

    Savva

--- src/app/lights/lightsone/page.jsx
+++ src/app/lights/lightsone/page.jsx
@@ -7,20 +7,16 @@ const page = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.blok}>
-        <div className={styles.textTsar}>
-          <h1>
+        <div className={styles.text}>
+          <div className={styles.textTsar}>
             17) И я видел, что Агнец Снял первую из семи печатей, и я услышал
             одно из четырёх животных, говорящее как бы громовым голосом: иди и
             смотри. 18) Я взглянул, и вот, конь белый, на нём всадник, имеющий
             лук, и дан был ему венец; и вышел он как победоносный, и чтобы
             победить.
-          </h1>
-          <Link href="/blagosds" className={styles.ruxyz}>
-            Благотворительный Фонд СДС и ОУ.
-          </Link>
-          <Link href="/lights" className={styles.ruxyz}>
-            Вернуться на главную.
-          </Link>
+          </div>
+          <Link href="/blagosds">Благотворительный Фонд СДС и ОУ.</Link>
+          <Link href="/lights">Вернуться на главную.</Link>
         </div>
       </div>
     </div>
--- src/app/tsarstvie/lawSha/page.jsx
+++ src/app/tsarstvie/lawSha/page.jsx
@@ -11,70 +11,77 @@ const page = () => {
       <div className={styles.blok}>
         <div className={styles.text}>
           <h4>
-            ПЕРЕВЕДИ СТРАНИЦУ! Как носитель престола и Его доверенное лицо, хочу
-            развеять ваши иллюзии, которыми вас питают те, кто не сильно
-            дальнозорок.
+            As the bearer of the throne and His confidant, I want to dispel your
+            illusions, which are fed to you by those who are not very
+            farsighted.
           </h4>
           <p>
-            1) Определитесь прежде за кем и куда вы идёте. Если идёте за
-            Христом, то исполняйте его закон. Если вы выбрали идти за кесарем,
-            то забудьте о Христе. Иисус и кесарь, это два разных направления.
-            Дорога ведущая за Христом ведёт в царствие. Если вы идёте за
-            кесарем, то вы выбрали дорогу в ад. Поэтому прежде, определитесь за
-            кем вы идёте. За Христом или за кесарем. Я говорю вам, вы напрасно
-            питаете себя иллюзиями и напрасно создали себе кумиров из лже отцов.
+            1) First of all, decide who you follow and where you're going. If
+            you follow Christ, then obey His law. If you choose to follow
+            Caesar, then forget about Christ. Jesus and Caesar are two different
+            directions. The path that leads to Christ leads to the Kingdom of
+            Heaven. If you follow Caesar, then you've chosen the path to hell.
+            Therefore, first of all, decide who you follow: Christ or Caesar. I
+            tell you, you feed on illusions in vain and create idols for
+            yourself from false fathers.
           </p>
           <p>
-            2) Пьющие, употребляющее, курящие, матершинники, психи, прочие, вы
-            реально себя считаете достойными Христа? Вы сильно заблуждаетесь,
-            этот ресурс вам откроет глаза и развеет ваши иллюзии. Знайте, если
-            на момент остановки вашего сердца вы не избавитесь от своих пагубных
-            привычек, то дорога вам в ад. Не стоит питать себя иллюзиями.
-            Праведник едва-едва спасается, много ли вы праведников встречали?
+            2) Drinkers, drug addicts, smokers, foul-mouthed people,
+            psychopaths, and others—do you really think you're worthy of Christ?
+            You're sorely mistaken. This resource will open your eyes and dispel
+            your illusions. Know this: if by the time your heart stops beating,
+            you haven't broken your bad habits, then your place is in hell.
+            Don't harbor any illusions. The righteous are hardly saved. How many
+            righteous people have you met?
           </p>
           <p>
-            3) Разведённые без вины прелюбодеяния, возвращайтесь к своим
-            половинкам или катитесь в ад, другого варианта для вас нет и
-            молитесь, чтобы те кого вы кинули не были обручены.
+            3) Pray that those you abandoned are not engaged. Divorce without
+            the guilt of adultery is prohibited. Union with divorced people
+            without the guilt of adultery is prohibited. Marriage with those
+            guilty of adultery is prohibited. A victim of adultery is free from
+            the law and can marry again.
           </p>
           <p>
-            4) Убийцы, неважно кто вы и при каких условиях вы убили (Случайно
-            или война, или защита или ещё что-либо придумаете), вам нет места в
-            царствии, как и в церкви, ваш дом ад на вечно гореть в огне. Я
-            проклинаю тех священнослужителей нелюдей кто вас предал, вы
-            разделите с ними награду.
+            4) Murderers, no matter who you are or under what circumstances you
+            killed (accident, war, self-defense, or whatever), there is no place
+            for you in the Kingdom of God or the Church. Your home, hell, will
+            burn in flames forever. I curse those inhuman priests who betrayed
+            you; you will share their punishment.
           </p>
           <p>
-            5) Данной мне властью говорю вам. На данном этапе я не вижу церкви в
-            которой проповедуются воскресение Христово. На днях ваше
-            православная школа, как и ряд других будут упразднены, как и ветхий
-            кровавый завет Моисея, а вместе с ним я переломаю хребет колеса
-            Иакова.
+            5) By virtue of the authority vested in me, I appeal to you. At this
+            moment, I do not see a single church where the resurrection of
+            Christ is preached. In a few days, your Catholic school, along with
+            many others, will be abolished, as will the Old Testament of Moses,
+            and with it, I will break Jacob's wheel.
           </p>
           <p>
-            Мой добрый друг. Делай всё по закону. Всё что велят вам делать и
-            соблюдать, делайте и соблюдайте что не противоречит закону Христа.
-            Поверьте, соблюдая закон Христа вы автоматически соблюдаете и закон
-            мирской. Помните, божье – это божье. Кесарево – это цари,
-            президенты, фараоны, князья. Убийца, как и прелюбодеи не могут
-            пожертвовать собой за друзей своих, это привилегия праведников.
+            My good friend. Do everything according to the law. Whatever you are
+            commanded to do and observe, do and observe it, unless it conflicts
+            with the law of Christ. Believe me, by keeping the law of Christ,
+            you automatically observe the law of the world. Remember, what
+            belongs to God belongs to God. What belongs to Caesar belongs to
+            kings, presidents, pharaohs, and princes. A murderer, like an
+            adulterer, cannot sacrifice himself for his friends; that is the
+            privilege of the righteous.
           </p>
         </div>
         <div className={styles.textTsar}>
           <p>
-            Вы сами виноваты в том, что не хотите слушать тех кого нужно. Идите,
-            задавайте вопросы тем, кто вас так научил. Всех тех, кто разделяет
-            мои с Иисусом ценности прошу пройти список вопросов по итогу
-            которого вы узнаете можете ли вы быть там где мы с Христом или вам
-            ещё учиться и учиться.{" "}
+            You yourself are to blame for not wanting to listen to the right
+            people. Go, ask questions to those who taught you this. I ask
+            everyone who shares my values ​​with Jesus to answer this list of
+            questions. By the end of this list, you will understand whether you
+            can be where Christ and I are, or whether you still have much to
+            learn.{" "}
             <a href="/tsarstvie/lawSha/lawOneSha" className={styles.optuw}>
-              Ответить.
+              Answer.
             </a>
           </p>
         </div>
       </div>
       <button className={styles.buttonla}>
-        <a href="/tsarstvie/1">Назад.</a>
+        <a href="/tsarstvie/2">Back.</a>
       </button>
     </div>
   );
