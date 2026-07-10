a7fee51d2814e9ad9309511c36d396477dcee63c
Savva <imperiya66.ru@gmail.com>
2026-07-10 23:39:13 +0300

    Savva10/07

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "The Throne of David The Kingdom of Jesus Christe",
-  description: "The word that Isaiah the son of Amoz saw concerning Judah and Jerusalem.And it shall come to pass in the last days, that the mountain of the LORD s house shall be established in the top of the mountains, and shall be exalted above the hills; and all nations shall flow unto it. And many people shall go and say, Come ye, and let us go up to the mountain of the LORD, to the house of the God of Jacob; and he will teach us of his ways, and we will walk in his paths: for out of Zion shall go forth the law, and the word of the LORD from Jerusalem.",
+  title: "Престол Давида Царство Иисуса Христа",
+  description:
+    "The word that Isaiah the son of Amoz saw concerning Judah and Jerusalem.And it shall come to pass in the last days, that the mountain of the LORD s house shall be established in the top of the mountains, and shall be exalted above the hills; and all nations shall flow unto it. And many people shall go and say, Come ye, and let us go up to the mountain of the LORD, to the house of the God of Jacob; and he will teach us of his ways, and we will walk in his paths: for out of Zion shall go forth the law, and the word of the LORD from Jerusalem.",
 };
 
 export default function RootLayout({ children }) {
